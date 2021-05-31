a8dda9c55e046081e0186d6a90c165efc224eb1f
leroy <leroythelegend@gmail.com>
2021-06-01 07:15:42 +1000

    remove build dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leigh/Projects/rough_idea_project_cars_cpp
-# Build directory: /home/leigh/Projects/rough_idea_project_cars_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
