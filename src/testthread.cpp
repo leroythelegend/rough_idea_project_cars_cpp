971cd702ae9a239bf79dde0419f6ee413b963fa3
leroy <leroythelegend@gmail.com>
2021-06-29 16:13:12 +1000

    try again

--- src/testthread.cpp
+++ src/testthread.cpp
@@ -2,18 +2,21 @@
 #include <string>
 #include <iostream>
 
+#include "../inc/processv2csvimpl.h"
+
 using namespace std;
+using namespace pcars;
 
 int main()
 {
 
     string trackname_("tracke");
     int currentlap_ = 1;
-    unique_ptr<string> data_ = make_unique<string>("data");
+    unique_ptr<TelemetryData> data_ = make_unique<TelemetryData>();
     string type_("type");
 
-    thread t([](string name, int lap, unique_ptr<string> data, const string &type)
-             { cout << name << " " << lap << " " << *data << type; },
+    thread t([](string name, int lap, unique_ptr<TelemetryData> data, const string &type)
+             { cout << name << " " << lap << " " << type; },
              trackname_, currentlap_, move(data_), type_);
     t.join();
 
