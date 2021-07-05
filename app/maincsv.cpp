d2ba5663d849c076373543a8d4d93b6f87300028
leroy <leroythelegend@gmail.com>
2021-07-05 15:43:59 +1000

    add command line args

--- app/maincsv.cpp
+++ app/maincsv.cpp
@@ -22,51 +22,83 @@ using namespace std;
 
 void runThread(const Telemetry::Ptr &, const ProcessV2CSVImpl::Ptr &);
 
-int main(int argc, char *argv[]) {
- 
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVTyreImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVSupImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVInputsImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVEngineImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVForceImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVRaceLineImpl>());
-	runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVWeatherImpl>());
-	runThread(make_shared<TelemetryMSG>(), make_shared<ProcessV2CSVMSGImpl>());
+int main(int argc, char *argv[])
+{
 
-	thread quit([](){
-		cout << "Press \'q\' then \'Enter\' to QUIT." << endl;
-		while (true)
-		{
-			char key;
-			cin >> key;
-			if (key == 'q')
-			{
-				return 0;
-			}
-		} 
-	});
+	string arg;
+	if (argc == 2)
+	{
+		arg.assign(argv[1], 3);
+	}
+	else
+	{
+		cout << "pcars [-v1 or -v2]" << endl;
+		cout << "Default to Version 2" << endl;
+		arg = "-v2";
+	}
+
+	if (arg == "-v1")
+	{
+		cout << "Version 1" << endl;
+	}
+	else if (arg == "-v2")
+	{
+		cout << "Version 2" << endl;
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVTyreImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVSupImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVInputsImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVEngineImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVForceImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVRaceLineImpl>());
+		runThread(make_shared<TelemetryV2>(), make_shared<ProcessV2CSVWeatherImpl>());
+		runThread(make_shared<TelemetryMSG>(), make_shared<ProcessV2CSVMSGImpl>());
+	}
+	else
+	{
+		cout << "pcars [-v1 or -v2]" << endl;
+		return 0;
+	}
+
+	thread quit([]()
+				{
+					cout << "Press \'q\' then \'Enter\' to QUIT." << endl;
+					while (true)
+					{
+						char key;
+						cin >> key;
+						if (key == 'q')
+						{
+							return 0;
+						}
+					}
+				});
 
 	quit.join();
 
 	return 0;
 }
 
-
 void runThread(const Telemetry::Ptr &telemetry, const ProcessV2CSVImpl::Ptr &impl)
 {
-	thread t([](const Telemetry::Ptr &telemetry_, const ProcessV2CSVImpl::Ptr &impl_){
-		try {
-			telemetry_->start(make_shared<ProcessV2CSV>(impl_));
-		}
-		catch (PCars_Exception & e) {
-			cout << e.what() << endl;
-		}
-		catch (exception & e) {
-			cout << e.what() << endl;
-		}
-		catch (...) {
-			cout << "Unkown error" << endl;
-		}
-	},telemetry ,impl);
-	t.detach();	
+	thread t([](const Telemetry::Ptr &telemetry_, const ProcessV2CSVImpl::Ptr &impl_)
+			 {
+				 try
+				 {
+					 telemetry_->start(make_shared<ProcessV2CSV>(impl_));
+				 }
+				 catch (PCars_Exception &e)
+				 {
+					 cout << e.what() << endl;
+				 }
+				 catch (exception &e)
+				 {
+					 cout << e.what() << endl;
+				 }
+				 catch (...)
+				 {
+					 cout << "Unkown error" << endl;
+				 }
+			 },
+			 telemetry, impl);
+	t.detach();
 }
