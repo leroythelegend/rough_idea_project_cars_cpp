80c0afac99ecb75309df8d3e9a951a9d878f67e9
leroy <leroythelegend@gmail.com>
2021-07-11 19:41:25 +1000

    add testdecoder2bit3bit

---
+++ tests/testdecoder2bit3bit.cpp
@@ -0,0 +1,42 @@
+#include "../inc/decoder2bit3bit.h"
+
+#include "tests.h"
+
+using namespace pcars;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    Decoder2bit3bit decoder;
+
+    // Test we throw out_of_range
+    PCars_Data data;
+    Position pos = 0;
+    try
+    {
+        decoder.decode(data, pos);
+        assert(false);
+    }
+    catch (out_of_range &e) 
+    {
+        // test pos is unchanged
+        assert(!pos);
+    }
+
+    // Test all values are 0
+    data.push_back(0b11001000);
+    decoder.decode(data, pos);
+    assert(pos == 1);
+    assert(decoder.ms2bits() == 0);
+    assert(decoder.ls3bits() == 0);
+ 
+    // Test ms2 bits and ls 3bits
+    data.at(0) = 0b00110111;
+    pos = 0;
+    decoder.decode(data, pos);
+    assert(pos == 1);
+    assert(decoder.ms2bits() == 3);
+    assert(decoder.ls3bits() == 7); 
+
+    return 0;
+}
